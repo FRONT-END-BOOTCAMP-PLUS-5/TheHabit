63e03106bf85425423cb1a6128b08158a4d4300b
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-08-04 17:01:32 +0900

    feat : tabNavBar UI 컴포넌트 제작
    issue#6

---
+++ app/feedback/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TabNavigation from "../_components/TabNavigation/TabNavigation";
+
+export default function FeedbackPage() {
+  return (
+    <div className="w-full h-full">
+      <TabNavigation />
+    </div>
+  );
+}
