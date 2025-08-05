3cad3a6914d38de0fbe36ea1564d29c0f4f55c9d
dongwookim <132253329+devdongwoo@users.noreply.github.com>
2025-08-05 14:20:45 +0900

    Update UserEntity.ts

--- backend/users/domains/entities/UserEntity.ts
+++ backend/users/domains/entities/UserEntity.ts
@@ -1,9 +1,9 @@
 export class User {
   constructor(
-    public readonly id: string,
     public readonly username: string,
     public readonly nickname: string,
     public readonly profileImg: string | null,
+    public readonly id?: string,
     public readonly password?: string,
     public readonly email?: string,
     public readonly createdAt?: string,
