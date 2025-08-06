f79c2fb3a805dc4b4754c00160264913a931db83
devdongwoo <gimdongwoo64@gmail.com>
2025-08-06 23:07:47 +0900

    템플릿 리터럴을 사용한 메소드인 $queryRaw에 alias 사용 + dto + CreateUserUsecase/ 상현님 사용하시라고 생성

--- backend/users/infrastructures/repositories/PrUserRepository.ts
+++ backend/users/infrastructures/repositories/PrUserRepository.ts
@@ -3,25 +3,26 @@ import { User } from "@/backend/users/domains/entities/UserEntity";
 import prisma from "@/public/utils/prismaClient";
 
 export class PrUserRepository implements IUserRepository {
-
   async create(user: User): Promise<User | undefined> {
     try{
       // prisma 쿼리문 사용법
       // insert, update, select 등 밑에 해놓음 예제
-      const createdUser = await prisma.$queryRaw`
-            INSERT INTO users 
+      const createdUser:User = await prisma.$queryRaw`
+            INSERT INTO users (email, nickname, password, username, profile_img)
             VALUES (
                 ${user.email}, 
                 ${user.nickname},
                 ${user.password}, 
-                ${user.username}, 
-            );`
+                ${user.username},
+                ${user.profileImg},
+                )
+            RETURNING id, email, nickname, username, profile_img as "profileImg";`
 
       return new User(
-          (createdUser as any).id,
-          (createdUser as any).username,
-          (createdUser as any).nickname,
-          (createdUser as any).profileImg
+          createdUser.username,
+          createdUser.nickname,
+          createdUser.profileImg,
+          createdUser.id,
       );
     }catch(e){
       if(e instanceof  Error) throw new Error(e.message)
@@ -30,21 +31,21 @@ export class PrUserRepository implements IUserRepository {
 
   async findAll(nickname: string = ''): Promise<User[] | undefined> {
     try{
-      const users = await prisma.$queryRaw`
+      const users:User[] = await prisma.$queryRaw`
         SELECT id,
            username,
            nickname,
-           profile_img
+           profile_img as "profileImg"
         FROM users
         WHERE 1=1
         AND nickname like '%${nickname}%'
         ;`
 
-      return (users as any[]).map((user: any) => new User(
-          user.id || '',
+      return users.map((user: User) => new User(
           user.username,
           user.nickname,
-          user.profileImg || ''
+          user.profileImg || '',
+          user.id || '',
       ));
     }catch(e){
       if(e instanceof  Error) throw new Error(e.message)
@@ -54,11 +55,11 @@ export class PrUserRepository implements IUserRepository {
 
   async findById(id: string): Promise<User | null | undefined> {
     try{
-      const user = await prisma.$queryRaw`
+      const user: User = await prisma.$queryRaw`
         SELECT id,
                username,
                nickname,
-               profile_img
+               profile_img as "profileImg"
         FROM users
         WHERE 1=1
         AND id = ${id}
@@ -67,10 +68,10 @@ export class PrUserRepository implements IUserRepository {
       if (!user) return null;
 
       return new User(
-          (user as any).id,
-          (user as any).username,
-          (user as any).nickname,
-          (user as any).profileImg
+          user.username,
+          user.nickname,
+          user.profileImg,
+          user.id,
       );
     }catch(e){
       if(e instanceof  Error) throw new Error(e.message)
