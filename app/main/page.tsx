27a1d1fd4de747ce24d7f29a66b5b68e6a4eeab0
HarenKei <seungmin1266@gmail.com>
2025-08-10 15:30:54 +0900

    feat : 유저 프로필 섹션 구현

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,8 +1,10 @@
 import WeeklySlide from "@/app/_components/weekly-slides/WeeklySlide";
+import UserProfileSection from "../_components/user-profile-section/UserProfileSection";
 
 const MainPage: React.FC = () => {
   return (
     <main>
+      <UserProfileSection />
       <WeeklySlide />
     </main>
   );
