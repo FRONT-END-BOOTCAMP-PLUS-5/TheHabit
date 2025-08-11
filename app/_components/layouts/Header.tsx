9a755f026eb82669c9bb7485c1ed96cf016c5110
HarenKei <seungmin1266@gmail.com>
2025-08-11 12:28:42 +0900

    style : 최장 날짜 스트릭에 그라디언트 효과 적용

--- app/_components/layouts/Header.tsx
+++ app/_components/layouts/Header.tsx
@@ -11,9 +11,11 @@ const Header: React.FC = () => {
       <h1 className={"text-3xl font-black text-primary flex-3"}>
         <Link href="/">The:Habit</Link>
       </h1>
-      <button className="flex flex-2 items-center justify-end gap-2 pr-4 cursor-pointer">
+      <button className="flex flex-2 items-center justify-end gap-1 pr-4 cursor-pointer">
         <Image src={FireIcon} alt="fire" width={24} height={24} />
-        <div className="text-xl font-bold text-black">21일</div>
+        <div className="text-xl font-extrabold bg-gradient-to-r from-[#FF6D00] via-[#FF9800] to-[#FFC107] bg-clip-text text-transparent">
+          21
+        </div>
       </button>
     </header>
   );
