c025068426d131fa7d843a84bc7806494aa19278
DevYsh <ysh940129@gmail.com>
2025-08-17 19:55:57 +0900

    refact: SocialLogin.tsx

--- app/login/components/SocialLogin.tsx
+++ app/login/components/SocialLogin.tsx
@@ -11,25 +11,23 @@ export const SocialLogin = () => {
     <div className='flex flex-col w-full gap-3'>
       <h3 className='text-xl text-center'>SNS 계정으로 로그인하기</h3>
       <div className='flex flex-row gap-10 justify-center'>
-        <button onClick={() => signIn('google', { callbackUrl: '/' })}>
           <Image
             src='/icons/google.svg'
             alt='google 로그인'
             width={60}
             height={60}
             className='cursor-pointer hover:scale-110 transition-all duration-300'
+            onClick={() => signIn('google', { callbackUrl: '/' })}
           />
-        </button>
 
-        <button onClick={() => signIn('kakao', { callbackUrl: '/' })}>
           <Image
             src='/icons/kakao.svg'
             alt='kakao 로그인'
             width={60}
             height={60}
             className='cursor-pointer hover:scale-110 transition-all duration-300'
+            onClick={() => signIn('kakao', { callbackUrl: '/' })}
           />
-        </button>
       </div>
     </div>
   );
