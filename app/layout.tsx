b7fcc7bbdcada1e01b5274954a2dce7b40d783a5
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-08-25 23:14:09 +0900

    refact : 레이아웃 프로바이더 import alias로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import ModalProvider from '@/app/_components/providers/ModalProvider';
 import NextAuthSessionProvider from '@/app/_components/providers/NextAuthSessionProvider';
 import { pretendard } from '@/public/fonts/font';
 import ToastProvider from '@/app/_components/providers/ToastProvider';
-import LayoutProvider from './_components/providers/LayoutProvider';
+import LayoutProvider from '@/app/_components/providers/LayoutProvider';
 
 export const metadata: Metadata = {
   title: 'TheHabit - 습관 관리 앱',
