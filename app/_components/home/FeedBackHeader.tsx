5f027ccbcd846bdd3185e01c592114b2b0b47450
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-09-05 21:56:16 +0900

    refact : nickname props 중복 제거

--- app/_components/home/FeedBackHeader.tsx
+++ app/_components/home/FeedBackHeader.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { useGetUserInfo } from '@/libs/hooks/user-hooks/useGetUserInfo';
+import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 
 export const FeedBackHeader = () => {
-  const { userInfo } = useGetUserInfo();
-  const nickname = userInfo?.nickname;
-  const username = userInfo?.username;
+  const { data: session } = useSession();
+  const nickname = session?.user?.nickname;
   const pathname = usePathname();
 
   const isFeedbackListPage = nickname ? pathname === `/user/feedback/${nickname}` : false;
@@ -18,7 +17,7 @@ export const FeedBackHeader = () => {
 
   return (
     <header className='flex items-center h-1/12 gap-2 justify-center relative mt-5'>
-      <h1 className='text-2xl font-bold'>{`'${username}'`}님의 성장 기록</h1>
+      <h1 className='text-2xl font-bold'>{`'${nickname}'`}님의 성장 기록</h1>
     </header>
   );
 };
